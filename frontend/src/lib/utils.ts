1a25035082e59a5cae64f4786addaa42cc05025b
Sergio Beleña <sergio.belena@wunderdog.fi>
2025-01-24 08:32:00 +0000

    🐛 Fix undefined values on formatCurrency

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,9 +1,10 @@
-export const formatCurrency = (value: number) => {
+export const formatCurrency = (value?: number) => {
+  if (!value) return null;
+
   if (value >= 1_000_000_000) {
     return `$${(value / 1_000_000_000).toFixed(0)}B`;
   } else if (value >= 1_000_000) {
     return `$${(value / 1_000_000).toFixed(0)}M`;
-  } else {
-    return `$${Math.round(value)}`;
   }
+  return `$${Math.round(value)}`;
 };
